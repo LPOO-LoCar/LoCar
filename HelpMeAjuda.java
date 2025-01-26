14123512b0b3e23ae7820697ea009aabcad548c0
AninhaLeao-B <aninha_brasil1992@hotmail.com>
2025-01-26 15:22:48 -0300

    Add files via upload

---
+++ HelpMeAjuda.java
@@ -0,0 +1,45 @@
+package test5;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class HelpMeAjuda extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HelpMeAjuda frame = new HelpMeAjuda();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HelpMeAjuda() {
+		setTitle("Help-me!");
+		setSize(400,300);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
