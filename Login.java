c163d1cf0dafe795e09c788cb7411dd5925328ec
AninhaLeao-B <aninha_brasil1992@hotmail.com>
2025-01-26 15:28:17 -0300

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,128 @@
+package test5;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	private void abrirTelaPrincipal() {
+		JFrame telaPrincipal = new TelaPrincipal();
+		telaPrincipal.setSize(500,400);
+		telaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		telaPrincipal.setVisible(true);
+	}
+	
+	
+	public Login() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("LoCar");
+		lblNewLabel_1.setBounds(156, 13, 80, 36);
+		lblNewLabel_1.setFont(new Font("Robotronica", Font.PLAIN, 30));
+		lblNewLabel_1.setForeground(new Color(0, 0, 0));
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel = new JLabel("LoCar");
+		lblNewLabel.setForeground(new Color(255, 0, 0));
+		lblNewLabel.setBounds(154, 11, 80, 36);
+		lblNewLabel.setFont(new Font("Robotronica", Font.PLAIN, 30));
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("Locadora de Veículos");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_2.setBounds(128, 31, 156, 27);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Usuário:");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_3.setBounds(176, 107, 56, 27);
+		contentPane.add(lblNewLabel_3);
+		
+		textField = new JTextField();
+		textField.setBounds(143, 134, 124, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Fazer Login");
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_4.setBounds(166, 69, 87, 27);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Senha:");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_5.setBounds(180, 159, 47, 20);
+		contentPane.add(lblNewLabel_5);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(143, 182, 124, 20);
+		contentPane.add(textField_1);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String usuario = textField.getText();
+				String senha = textField_1.getText();
+				
+				if (usuario.equals("admin") && senha.equals("1234")) {
+					JOptionPane.showMessageDialog(null, "Login bem-sucedido!");
+					dispose();
+					abrirTelaPrincipal();
+				} else {
+					JOptionPane.showMessageDialog(null, "Usuário ou senha incorretos", "Erro", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnNewButton.setBounds(164, 213, 89, 23);
+		contentPane.add(btnNewButton);
+	}
+}
+
+
